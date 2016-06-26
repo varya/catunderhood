09a74ff7b17a33c9ae98c5c34d985df2364848bd
Varya Stepanova <mail@varya.me>
2016-06-26 22:03:07 +0300

    Fake first tweet

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    { username: 'n_snork',  start: '27 June 2016', first: '', post: false },
+    { username: 'n_snork',  start: '27 June 2016', first: '747072443420794880', post: false },
     { username: 'toivonens',  start: '20 June 2016', first: '739572113393233924', }
 ];
