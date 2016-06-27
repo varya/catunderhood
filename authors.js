9553f44542c0d785b89816c3929bc238b8ef5f48
Varya Stepanova <mail@varya.me>
2016-06-27 09:34:06 +0300

    New author

--- authors.js
+++ authors.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
+    { username: 'n_snork',  start: '27 June 2016', first: '747314404358660097', post: false },
     { username: 'toivonens',  start: '20 June 2016', first: '744777414010495000' }
 ];
