e7d77c073721b88348bf1b13aa70b2e225bfdccf
Varya Stepanova <mail@varya.me>
2016-07-04 10:47:24 +0300

    @n_snok Close

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    { username: 'n_snork',  start: '27 June 2016', first: '747314404358660097', post: false },
+    { username: 'n_snork',  start: '27 June 2016', first: '747314404358660097' },
     { username: 'toivonens',  start: '20 June 2016', first: '744777414010495000' }
 ];
