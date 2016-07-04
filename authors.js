582c54696fc28a1b9dbd70cb23fe48ddcb3adec4
Varya Stepanova <mail@varya.me>
2016-07-04 11:05:04 +0300

    Typo

--- authors.js
+++ authors.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    { username: 'ki_zu',  start: '04 July 2016', first: '749694962174873600', post: flase },
+    { username: 'ki_zu',  start: '04 July 2016', first: '749694962174873600', post: false },
     { username: 'n_snork',  start: '27 June 2016', first: '747314404358660097' },
     { username: 'toivonens',  start: '20 June 2016', first: '744777414010495000' }
 ];
