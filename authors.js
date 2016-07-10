1c1351366640687f29f5f2b50b9ac4fda89c5072
Varya Stepanova <mail@varya.me>
2016-07-10 22:43:06 +0300

    @blia shift starts

--- authors.js
+++ authors.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
+    { username: 'blia',  start: '11 July 2016', first: '752186778027778048', post: false },
     { username: 'ki_zu',  start: '04 July 2016', first: '749886512934821888' },
     { username: 'n_snork',  start: '27 June 2016', first: '747314404358660097' },
     { username: 'toivonens',  start: '20 June 2016', first: '744777414010495000' }
