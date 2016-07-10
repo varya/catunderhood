9e69d6655e8885f0bcd1bcdbb54b67aeccca3c8e
Varya Stepanova <mail@varya.me>
2016-07-10 22:19:40 +0300

    Close ki_zu shift

--- authors.js
+++ authors.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    { username: 'ki_zu',  start: '04 July 2016', first: '749694962174873600', post: false },
+    { username: 'ki_zu',  start: '04 July 2016', first: '749694962174873600' },
     { username: 'n_snork',  start: '27 June 2016', first: '747314404358660097' },
     { username: 'toivonens',  start: '20 June 2016', first: '744777414010495000' }
 ];
