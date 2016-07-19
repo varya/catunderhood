d183becc7781d3add5d554cf981f263a2210ee8d
Varya Stepanova <mail@varya.me>
2016-07-19 20:58:32 +0300

    Correct first tweet for @katyabutenko

--- authors.js
+++ authors.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    { username: 'katyabutenko',  start: '18 July 2016', first: '754765567232442368', post: false },
+    { username: 'katyabutenko',  start: '18 July 2016', first: '755000338151800832', post: false },
     { username: 'blia',  start: '11 July 2016', first: '752394486496555008' },
     { username: 'ki_zu',  start: '04 July 2016', first: '749886512934821888' },
     { username: 'n_snork',  start: '27 June 2016', first: '747314404358660097' },
