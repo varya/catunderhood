68129b9963b37e3fb479c6845b00ec1c0cc9cc6e
Varya Stepanova <mail@varya.me>
2016-07-25 10:39:37 +0300

    @mdevils shift starts

--- authors.js
+++ authors.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
+    { username: 'mdevils',  start: '25 July 2016', first: '757327244746293248', post: false },
     { username: 'katyabutenko',  start: '18 July 2016', first: '755000338151800832' },
     { username: 'blia',  start: '11 July 2016', first: '752394486496555008' },
     { username: 'ki_zu',  start: '04 July 2016', first: '749886512934821888' },
