d2d592b174c84313e14b99bd4994783845d9774d
Varya Stepanova <mail@varya.me>
2016-08-08 13:45:34 +0300

    @realzzebra end of shift

--- authors.js
+++ authors.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    { username: 'realzzebra',  start: '1 August 2016', first: '760123417098149888', post: false },
+    { username: 'realzzebra',  start: '1 August 2016', first: '760123417098149888' },
     { username: 'mdevils',  start: '25 July 2016', first: '757505845873836037' },
     { username: 'katyabutenko',  start: '18 July 2016', first: '755000338151800832' },
     { username: 'blia',  start: '11 July 2016', first: '752394486496555008' },
