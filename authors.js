9f9a80aa72744ccf323dfafb2880fa3dfd64d304
Varya Stepanova <mail@varya.me>
2016-09-25 21:55:01 +0300

    Start with @yakowleva

--- authors.js
+++ authors.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
+    { username: 'yakowleva',  start: '26 September 2016', first: '780085384277483520', post: false },
     { username: 'sensovita',  start: '19 September 2016', first: '777701657467613188' },
     { username: 'olyapka',  start: '12 September 2016', first: '775282762551001088' },
     { username: 'mothersmonster',  start: '5 September 2016', first: '773022986643255298' },
