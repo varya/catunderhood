a5b70200c8b0512c5be770eb7f6d0020e4588b8a
Varya Stepanova <mail@varya.me>
2020-08-24 10:14:43 +0300

    End of shift

--- authors.js
+++ authors.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-multi-spaces */
 
 export default [
-    { username: 'DrKottof',  start: '10 August 2020', first: '1292552247180746757', post: false },
+    { username: 'DrKottof',  start: '10 August 2020', first: '1295284921636999169' },
     { username: 'kak6e',  start: '3 August 2020', first: '1290193197625299000' },
     { username: 'frefrenzy',  start: '20 July 2020', first: '1285260657617711000' },
     { username: 'cawushka',  start: '13 July 2020', first: '1282568529267564500' },
